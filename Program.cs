dcefe5ffa01c76848bfb963e74a6e3e44a9327a5
Kingkarn <Kingkarn.jant@mail.kmutt.ac.th>
2023-05-15 14:47:17 +0700

    save1

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        int NumOfCity = int.Parse(Console.ReadLine()); //NumOfCity คือจำนวนตัวแปรเมืองที่แสดงในแบบจำลอง
+        int[] City = new int[NumOfCity]; //City คือเมือง
+        for (int i = 0 ; i < NumOfCity ; i++) {
+           City[i] = int.Parse(Console.ReadLine());
+           DetailOfCity(City);
+        }
+    }
+    static void DetailOfCity(int[] City) { //DetailOfCity() คือฟังก์ชันรายละเอียดของเมือง
+        string NameOfCity = Console.ReadLine();
+        int NumOfContactCity = int.Parse(Console.ReadLine());
+        int[] ContactCity = new int[NumOfContactCity];
+        Contact(ContactCity , NumOfContactCity);
+    }
+    static void Contact(int[] ContactCity , int NumOfContactCity) {
+       while (true) {
+        ContactCity[0] = int.Parse(Console.ReadLine());
+       }
+
+    } 
+}
