d55e89901feec09af2fc41b36128fa3ca6572bd1
Kingkarn <Kingkarn.jant@mail.kmutt.ac.th>
2023-05-15 15:08:33 +0700

    save2

--- Program.cs
+++ Program.cs
@@ -3,22 +3,19 @@
 class Program {
     static void Main(string[] args) {
         int NumOfCity = int.Parse(Console.ReadLine()); //NumOfCity คือจำนวนตัวแปรเมืองที่แสดงในแบบจำลอง
-        int[] City = new int[NumOfCity]; //City คือเมือง
+        int[] City = new int[NumOfCity]; //City คือหมายเลขประจำเมือง
         for (int i = 0 ; i < NumOfCity ; i++) {
            City[i] = int.Parse(Console.ReadLine());
            DetailOfCity(City);
         }
     }
     static void DetailOfCity(int[] City) { //DetailOfCity() คือฟังก์ชันรายละเอียดของเมือง
-        string NameOfCity = Console.ReadLine();
-        int NumOfContactCity = int.Parse(Console.ReadLine());
-        int[] ContactCity = new int[NumOfContactCity];
+        string NameOfCity = Console.ReadLine(); //NameOfCity คือชื่อเมือง
+        int NumOfContactCity = int.Parse(Console.ReadLine());//NumOfContactCity คือจำนวนเมืองที่ติดต่อ
+        int[] ContactCity = new int[NumOfContactCity]; //ContactCity คือหมายเลขประจำเมืองที่ติดต่อ
         Contact(ContactCity , NumOfContactCity);
     }
-    static void Contact(int[] ContactCity , int NumOfContactCity) {
-       while (true) {
-        ContactCity[0] = int.Parse(Console.ReadLine());
-       }
-
+    static void Contact(int[] ContactCity , int NumOfContactCity) { //Contact คือฟังก์ชันติดต่อในข้อ 1.2 1.3
+      
     } 
 }
